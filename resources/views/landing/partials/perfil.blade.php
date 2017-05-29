ea82aef0dbcd2390795abe727fc82c610d2514b4
Nixon <nixontorres26@gmail.com>
2017-05-29 16:43:10 -0400

    perfil

--- resources/views/landing/partials/perfil.blade.php
+++ resources/views/landing/partials/perfil.blade.php
@@ -1,7 +1,10 @@
 <!-- Two -->
+@if($Perfil->visibility == "1")
 <section id="Perfil" class="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
 	<div class="content">
-		<h2>Dr. Carlos Pisanu.</h2>
+		@if(!empty($Cabecera->content->title))
+		<h2>{{ $Perfil->content->title }}</h2>
+		@endif
 		<p>Detalles sobre el resumen curricular, experiencia, trabajo, objetivos y visi&oacute;n.</p>
 		<ul class="actions vertical">
 			<li><a href="#" class="button">M&aacute;s informaci&oacute;n</a></li>
@@ -11,3 +14,4 @@
 		<img src="images/spotlight01.jpg" alt="" />
 	</div>
 </section>
+@endif
