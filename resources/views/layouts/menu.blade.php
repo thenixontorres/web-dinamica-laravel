1a9eb5e8207e93ed12ebaeec12c467b1acf70fd2
Nixon <nixontorres26@gmail.com>
2017-05-31 16:45:21 -0400

    test

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -22,6 +22,9 @@
     <a href="{!! route('services.index') !!}"><i class="fa fa-edit"></i><span>Servicios</span></a>
 </li>
 
+
+
+
 <!--<li class="{{ Request::is('socials*') ? 'active' : '' }}">
     <a href="{!! route('socials.index') !!}"><i class="fa fa-edit"></i><span>Redes Sociales</span></a>
 </li> -->
