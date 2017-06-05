0b1179eb8722126799ff67ee6ca997dd58dfcfde
Nixon <nixontorres26@gmail.com>
2017-06-05 16:22:04 -0400

    detalles maquetacion

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -2,35 +2,40 @@
 
 @section('content')
 <!-- Six -->
-<div class="row">
-	<div class="col-sm-9 col-sm-offset-1">					
-		<div class="row">
-			<div class="col-sm-10">		
-				<article class="wrapper style1 align-center">
-					<div class="inner">
-							<h2>{{ $article->title }}</h2>
-					</div>
-					<p class="align-left">{{ $article->tag->category }}</p>
-					<div class="image">
-						<img class="img img-responsive" src="{{ asset('/storage/images/').'/'.$article->thumb->thumb }}" alt="" />
-					</div>
-					<div class="inner text-justify">
-						<p>{!! $article->content !!}</p>
-					</div>
-				</article>
-				<article class="wrapper style1">
-					<p class="align-right">  {{ $article->created_at }} </p>
-				</article>
+<div class="container-fuid">
+	<div class="row">
+		<div class="col-sm-9 col-sm-offset-1">					
+			<div class="row">
+				<div class="col-sm-10">		
+					<article class="wrapper style1 align-center">
+						<div class="inner">
+								<h2>{{ $article->title }}</h2>
+						</div>
+						<p class="align-left">{{ $article->tag->category }}</p>
+						<div class="image">
+							<img class="img img-responsive" src="{{ asset('/storage/images/').'/'.$article->thumb->thumb }}" alt="" />
+						</div>
+						<div class="inner text-justify">
+							<p>{!! $article->content !!}</p>
+						</div>
+					</article>
+					<article class="wrapper style1">
+						<p class="align-right">  {{ $article->created_at }} </p>
+					</article>
+				</div>
 			</div>
 		</div>
+		<div class="col-sm-2">
+			<div class="row">
+			@include('blog.partials.category')
+			</div>
+		</div>					
 	</div>
-	<div class="col-sm-2">
-		<div class="row">
-		@include('blog.partials.category')
+
+	<div class="row">	
+		<div class="col-sm-10 col-sm-offset-1">				
+		@include('landing.partials.footer')	
 		</div>
-	</div>					
-</div>
-
-@include('landing.partials.footer')
-
+	</div>	
+</div>	
 @endsection
--- resources/views/blog/public.blade.php
+++ resources/views/blog/public.blade.php
@@ -2,42 +2,48 @@
 
 @section('content')
 <!-- Six -->
-<div class="row">
-	<div class="col-sm-9 col-sm-offset-1">	
-	@if(isset($articles))				
-		@foreach($articles as $article)
-		<div class="row">
-			<div class="col-sm-10">		
-				<article class="wrapper style1 align-center">
-					<div class="inner">
-							<h2><a href="{{ route('blog.article', $article->id) }}">{{ $article->title }}</h2></a>
-					</div>
-					<p class="align-left">{{ $article->tag->category }}</p>
-					<div class="image">
-						<a href="{{ route('blog.article', $article->id) }}">
-						<img class="img img-responsive" src="{{ asset('/storage/images/').'/'.$article->thumb->thumb }}" alt="" />
-						</a>
-					</div>
-					<div class="inner">
-						<p>{{ $article->summary }}</p>
-					</div>
-				</article>
-				<article class="wrapper style1">
-					<p class="align-right">  {{ $article->created_at }} </p>
-				</article>
+<div class="container-fuid">
+	<div class="row">
+		<div class="col-sm-9 col-sm-offset-1">	
+		@if(isset($articles))				
+			@foreach($articles as $article)
+			<div class="row">
+				<div class="col-sm-10">		
+					<article class="wrapper style1 align-center">
+						<div class="inner">
+								<h2><a href="{{ route('blog.article', $article->id) }}">{{ $article->title }}
+								</a></h2>
+						</div>
+						<p class="align-left">{{ $article->tag->category }}</p>
+						<div class="image">
+							<a href="{{ route('blog.article', $article->id) }}">
+							<img class="img img-responsive" src="{{ asset('/storage/images/').'/'.$article->thumb->thumb }}" alt="" />
+							</a>
+						</div>
+						<div class="inner">
+							<p>{{ $article->summary }}</p>
+						</div>
+					</article>
+					<article class="wrapper style1">
+						<p class="align-right">  {{ $article->created_at }} </p>
+					</article>
+				</div>
 			</div>
+			@endforeach
+		{{ $articles->links() }}
+		@endif
 		</div>
-		@endforeach
-	{{ $articles->links() }}
-	@endif
+		<div class="col-sm-2">
+			<div class="row">
+				@include('blog.partials.category')
+			</div>
+		</div>					
 	</div>
-	<div class="col-sm-2">
-		<div class="row">
-			@include('blog.partials.category')
+
+	<div class="row">	
+		<div class="col-sm-10 col-sm-offset-1">				
+		@include('landing.partials.footer')	
 		</div>
-	</div>					
+	</div>	
 </div>
-
-@include('landing.partials.footer')
-
 @endsection
